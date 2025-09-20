b04865c284950ae56e75c5bf92c8af384efbd6e8
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2025-09-20 12:41:26 +0200

    nucleus primary monitor

--- hosts/nucleus/home.nix
+++ hosts/nucleus/home.nix
@@ -13,16 +13,19 @@
   wayland.windowManager.hyprland.settings = {
     # Desktop dual monitor setup
     monitor = [
-      # 144Hz ASUS monitor on left, 60Hz Samsung on right
+      # 144Hz ASUS monitor on left (primary), 60Hz Samsung on right
       "DP-3,1920x1080@144,0x0,1.0"
       "HDMI-A-1,1920x1080@60,1920x0,1.0"
-      # Disable laptop monitor when not connected
-      "eDP-1,disable"
     ];
 
     # Workspace assignment for dual monitor
     workspace = [
       "1, monitor:DP-3, default:true"
+    ];
+
+    # Set cursor to primary monitor on startup
+    exec-once = [
+      "hyprctl dispatch workspace 1"
     ];
   };
 
