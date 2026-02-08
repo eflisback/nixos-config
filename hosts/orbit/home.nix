7c34eab0385ad306473f369faa9221fa16d4d5dd
Ebbe Flisbäck <ebbe.flisback@gmail.com>
2026-02-08 15:47:37 +0100

    Hyprland monitor config for orbit

--- hosts/orbit/home.nix
+++ hosts/orbit/home.nix
@@ -4,4 +4,14 @@
   imports = [
     ../../home/common.nix
   ];
+
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      "eDP-1,1920x1200@60,0x0,1"
+      ",preferred,auto,1"
+    ];
+    workspace = [
+      "1, monitor:eDP-1, default:true"
+    ];
+  };
 }
